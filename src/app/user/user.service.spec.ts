20bf5e774965bd8fea5a646f21369ecd5849e394
Dylan Tjabring <tjabringd@aforbes.com>
2026-03-17 13:22:03 +0200

    User Service Test

--- src/app/user/user.service.spec.ts
+++ src/app/user/user.service.spec.ts
@@ -4,7 +4,6 @@ import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(UserService);
@@ -13,4 +12,12 @@ describe('UserService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get users', () => {
+    service.getUsers().subscribe((users) => {
+      expect(users.length).toBeGreaterThan(0);
+    });
+  });
+
+  
 });
